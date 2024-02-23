7daad7b2c58f19ad068842e63ab8a43f2ef946ef
stoff1180 <stoffmustapha1180@gmail.com>
2024-02-23 15:04:44 -0800

    fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Install a version of flask (2.1.0) using pip3
-exec {'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "version: 2.1.0"'
+# Install a specific version of flask (2.1.0) using pip3
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
