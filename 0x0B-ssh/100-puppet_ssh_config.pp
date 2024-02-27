c7e6260a9872c956d27e29061c8ad365b5f83a62
stoff1180 <stoffmustapha1180@gmail.com>
2024-02-27 06:28:29 -0800

    puppet_ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,28 @@
 #!/usr/bin/env bash
 #using Puppet to make changes to our configuration file.#
 #connect to a server without typing a password.
+file { '/etc/ssh/ssh_config':
+    ensure  => present,
+    owner   => 'ubuntu',
+    mode    => '0644',
+    content => "
+Host 368074-web-01
+    Hostname 54.209.110.192
+    IdentityFile ~/.ssh/school
+    PreferredAuthentications publickey
+    PasswordAuthentication no
+",
+}
 
+file { '/home/ubuntu/.ssh_config':
+    ensure  => 'present',
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+    mode    => '0600',
+    content => "
+Host 368074-web-01
+    Hostname 54.209.110.192
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+",
+}
