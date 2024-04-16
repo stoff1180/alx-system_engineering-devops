6d43750dbd6d283a7968fdc2673d216bbc1a42b9
stoff1180 <stoffmustapha1180@gmail.com>
2024-04-16 14:31:09 +0000

    Sky is the limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# script increases the amount of traffic an Nginx server can handle
+# Increase the ULIMIT of the default file and restart Nginx
+
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# Change configuration to login with the holberton user and open a file.
+# Increase hard file limit for user holberton
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
